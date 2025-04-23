cb891e6c672b8d2d02126c27dda95d56a3a52a4d
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2025-04-23 13:16:56 +0700

    Add navbar items configuration to constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,28 @@
+export const navbarItems = [
+  {
+    title: "home",
+    href: "/",
+  },
+  {
+    title: "products",
+    href: "/products",
+    submenu: [
+      {
+        title: "gadget",
+        href: "/",
+      },
+      {
+        title: "drink",
+        href: "/",
+      },
+      {
+        title: "food",
+        href: "/",
+      },
+    ],
+  },
+  {
+    title: 'contact',
+    href: '/contact',
+  }
+]
