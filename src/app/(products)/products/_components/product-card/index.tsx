976b42edb8b10149e2da2a95b20f9aa71d29bd0f
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2025-05-24 23:48:07 +0700

    feat(product-card): refactor ProductCard component to use Product type and improve price formatting

--- src/app/(products)/products/_components/product-card/index.tsx
+++ src/app/(products)/products/_components/product-card/index.tsx
@@ -7,56 +7,27 @@ import { toast } from "sonner"
 import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 import { Button } from "@/components/ui/button"
-import { Rating } from "@smastrom/react-rating"
+
 import { useUserStore } from "@/store/use-user"
-import { cn } from "@/lib/utils"
+
+import { cn, formatPriceToRupiah } from "@/lib/utils"
+import { Product } from "@/lib/types/product"
+import { useFilters } from "@/store/use-filters"
+import { useRouter } from "next/navigation"
+import Tooltips from "@/components/tooltips"
+import { ArrowUpRight } from "lucide-react"
 
 interface ProductCardProps {
-  id: number
-  title: string
-  price: number
-  originalPrice?: number
-  image: string
-  category?: string
-  rating: number
-  reviews: number
-  badge?: {
-    text: string
-    type: "hot" | "trending" | "sale" | "discount"
-  }
-  className?: string
+  product: Product
 }
-
-export default function ProductCard({
-  id,
-  title,
-  price,
-  originalPrice,
-  image,
-  category,
-  rating,
-  reviews,
-  badge,
-  className
-}: ProductCardProps) {
+export default function ProductCard({ product }: ProductCardProps) {
   const { isAuthenticated } = useUserStore();
+  const { setCategory } = useFilters();
+  const router = useRouter();
 
-  const getBadgeColor = (type: string) => {
-    switch (type) {
-      case "hot":
-        return "bg-red-500"
-      case "trending":
-        return "bg-green-500"
-      case "sale":
-        return "bg-blue-500"
-      case "discount":
-        return "bg-orange-500"
-      default:
-        return "bg-gray-500"
-    }
-  }
+  const image = product?.coverImage ?? product?.multipleImage?.[0]?.url
 
-  function handleSellNow(id: number): void {
+  function handleSellNow(id: string): void {
     console.log("Sell Now clicked", id)
     if (isAuthenticated) {
       toast.success("Berhasil menambah produk ke keranjang jual!", { richColors: true })
@@ -70,7 +41,6 @@ export default function ProductCard({
     <Card className={
       cn(
         "group relative overflow-hidden transition-all duration-300 shadow-none hover:shadow-lg grid grid-cols-1 h-fit",
-        className
       )
     }>
       <CardContent className="p-0">
@@ -78,21 +48,13 @@ export default function ProductCard({
         <div
           className="relative overflow-hidden"
         >
-          {badge && (
-            <span
-              className={`absolute left-3 top-3 z-10 rounded-md ${getBadgeColor(badge.type)} px-2 py-1 text-xs text-white`}
-            >
-              {badge.text}
-            </span>
-          )}
-
-          <Link href={`/products/${title.replaceAll(' ', '-')}`} className="block">
+          <Link href={`/products/${product.id}`} className="block">
             <div className="relative h-64 overflow-hidden">
               <Image
-                src={image || "/placeholder.svg?height=200&width=200"}
-                alt={title}
+                src={image === '' ? "https://placehold.co/600x400" : image}
+                alt={product.name}
                 fill
-                className="object-contain transition-transform duration-300 group-hover:scale-105"
+                className="object-cover transition-transform duration-300 group-hover:scale-105"
               />
             </div>
           </Link>
@@ -102,25 +64,29 @@ export default function ProductCard({
         {/* Product Info area */}
         <div className="p-4 space-y-4">
           <div className="mb-2">
-            {category && <div className="mb-1 text-sm text-blue-500">{category}</div>}
-            <h3 className="mb-1 text-sm md:text-base font-medium line-clamp-2">{title}</h3>
-            <div className="flex items-center">
-              <Rating
-                className="h-5 w-5 fill-yellow-400 text-yellow-400"
-                value={rating}
-                readOnly
-                style={{ width: 100 }}
-              />
-              <span className="ml-1 text-xs md:text-base text-gray-500">({reviews})</span>
-            </div>
+            <Tooltips
+              description="Klik untuk melihat produk serupa"
+              side="right"
+            >
+              <span
+                onClick={() => {
+                  setCategory(product.category!);
+                  router.push(`/products?category=${product.category}`);
+                }}
+                className="mb-1 text-blue-400 cursor-pointer inline-flex gap-2"
+              >
+                {product.category} <ArrowUpRight />
+              </span>
+            </Tooltips>
+            <h3 className="mb-1 text-sm md:text-base font-semibold line-clamp-2 capitalize">{product.name}</h3>
           </div>
 
           <div>
             <h6 className="text-gray-400 font-medium text-xs md:text-sm">Harga Modal</h6>
             <div className="flex items-center">
-              <span className="text-sm md:text-base font-medium">${price.toFixed(2)}</span>
-              {originalPrice && (
-                <span className="ml-2 text-xs text-gray-500 line-through">${originalPrice.toFixed(2)}</span>
+              <span className="text-sm md:text-base font-medium">{formatPriceToRupiah(product.amount)}</span>
+              {formatPriceToRupiah(product.amount) && (
+                <span className="ml-2 text-xs text-gray-500 line-through">{formatPriceToRupiah(product.amount)}</span>
               )}
             </div>
           </div>
@@ -128,7 +94,7 @@ export default function ProductCard({
           <div>
             <h6 className="text-gray-400 font-medium text-xs md:text-sm">Rekomendasi harga jual</h6>
             <div className="flex items-center">
-              <span className="text-sm md:text-base font-medium text-orange-500">${price.toFixed(2)} - ${price.toFixed(2)}</span>
+              <span className="text-sm md:text-base font-medium text-orange-500">{formatPriceToRupiah(product.amount)} - {formatPriceToRupiah(product.amount)}</span>
             </div>
           </div>
         </div>
@@ -136,7 +102,12 @@ export default function ProductCard({
       </CardContent>
       <CardFooter className="h-fit self-end">
         {/* Action */}
-        <Button className="w-full uppercase py-2 border-primary text-primary font-bold hover:bg-blue-500/10" variant="outline" size="lg" onClick={() => handleSellNow(id)}>
+        <Button
+          variant='secondary'
+          className="w-full uppercase"
+          size="lg"
+          onClick={() => handleSellNow(product?.id)}
+        >
           Jual Sekarang
         </Button>
         {/* Action */}
