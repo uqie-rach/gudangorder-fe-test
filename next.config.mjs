fda1af57d3378c5c9a79857af999c957a02cbc84
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2025-05-24 23:45:21 +0700

    chore: update dependencies in package.json
    - Added new dependencies:
      - @hookform/resolvers
      - @radix-ui/react-avatar
      - @radix-ui/react-label
      - @radix-ui/react-scroll-area
      - @radix-ui/react-tabs
      - @tanstack/react-query
      - react-hook-form
      - react-to-pdf
      - zod
      - @tailwindcss/typography
    
    - Updated existing dependencies to latest versions.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    dangerouslyAllowSVG: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn.dummyjson.com',
         port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.mayar.id',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
       }
     ]
   }
